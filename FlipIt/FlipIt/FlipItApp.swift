79beda3302f3d254fb3292d730f512f4435d9acc
Arthur Lopes <arthurpsl99@gmail.com>
2023-08-23 18:53:37 -0300

    Initial commit

---
+++ FlipIt/FlipIt/FlipItApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlipItApp.swift
+//  FlipIt
+//
+//  Created by Arthur Sobrosa on 23/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FlipItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
